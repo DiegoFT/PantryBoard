b2bf987fa670297f31fb7f1daa2b751dcc0e065b
DiegoFT <diegoft87@gmail.com>
2022-06-15 22:07:16 +0200

    update

---
+++ src/main/java/com/pantryboard/PantryBoardApplication.java
@@ -0,0 +1,13 @@
+package com.pantryboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantryBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pantryboard/PantryBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pantryboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
